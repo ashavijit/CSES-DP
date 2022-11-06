d978c70176bf69037ce5f33431bda2a85b7065f6
ashavijit <as2440@it.jgec.ac.in>
2022-11-07 00:06:33 +0530

    better code

--- DiceComb.cpp
+++ DiceComb.cpp
@@ -1,28 +1,23 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
 
-#define MAXN 1000001
-#define MOD 1000000007
 
-int DP[MAXN], n;
-
-int compute (int left) {
-    if (DP[left] != 0) {
-        return DP[left];
-    }
-    for (int i = 1; i <= 6; i++) {
-        if (left - i >= 0) {
-            DP[left] += compute(left - i);
-            DP[left] %= MOD;
+int main()
+{
+    int mod = 1e9+7;
+    int n;
+    cin>>n;
+    vector<int> dp(n+1,0);
+    dp[0] = 1;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=6;j++)
+        {
+            if(i-j>=0)
+            {
+                dp[i] = (dp[i] + dp[i-j])%mod;
+            }
         }
     }
-    return DP[left];
-}
-
-int main() {
-    cin >> n;
-    memset(DP, 0, sizeof(DP));
-    DP[0] = 1;
-    cout << compute(n) << endl;
+    cout<<dp[n]<<endl;
 }
